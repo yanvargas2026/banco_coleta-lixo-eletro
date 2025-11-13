5499fc2f2dd0242cefdc7d1f4ee77729f18f32d8
yanvargas2026 <yanderson.moraes@ufms.br>
2025-11-13 19:25:55 -0400

    inseridos ao menos 3 registros nas tabelas

--- banco_coleta_lixo.sql
+++ banco_coleta_lixo.sql
@@ -61,3 +61,32 @@ CREATE TABLE integracoes_prefeitura (
     
     FOREIGN KEY (id_solicitacao) REFERENCES solicitacoes(id_solicitacao)
 );
+
+-- Inserindo dados dos usuarios (3 registros)
+INSERT INTO usuarios (nome, email, senha_hash, telefone) VALUES
+('Ana Silva', 'ana@example.com', 'hash_senha_teste_1', '11987654321'),
+('Carlos Mendes', 'carlos@example.com', 'hash_senha_teste_2', '11988887777'),
+('Fernanda Rocha', 'fernanda@example.com', 'hash_senha_teste_3', NULL);
+
+-- inserindo dados das solicitações (3 registros)
+INSERT INTO solicitacoes (id_usuario, nome, endereco, tipo_lixo, status)
+VALUES
+(1, 'Ana Silva', 'Rua das Palmeiras, 101', 'Celular', 'Aguardando'),
+(2, 'Carlos Mendes', 'Av. Brasil, 2000', 'Notebook', 'Recebida'),
+(3, 'Fernanda Rocha', 'Rua Nova, 55', 'TV', 'Cancelamento Solicitado');
+
+-- Inserindo os tipos status (3 registros)
+INSERT INTO logs_status (id_solicitacao, status_anterior, status_novo, motivo)
+VALUES
+(1, 'Aguardando', 'Recebida', 'Coletor registrou recebimento'),
+(2, 'Recebida', 'Cancelamento Solicitado', 'Usuário solicitou cancelamento'),
+(3, 'Aguardando', 'Cancelado', 'Cancelamento automático pelo sistema');
+
+-- Inserindo as insformações retornadas na integração (3 registros)
+INSERT INTO integracoes_prefeitura (
+    id_solicitacao, recebido_em, status_prefeitura, protocolo_prefeitura, mensagem_retorno
+)
+VALUES
+(1, NOW(), 'Confirmado', 'PRT-2025-001', 'Solicitação recebida com sucesso'),
+(2, NOW(), 'Recusado', 'PRT-2025-002', 'Endereço fora da área de cobertura'),
+(3, NULL, NULL, NULL, 'Aguardando envio para a prefeitura');
